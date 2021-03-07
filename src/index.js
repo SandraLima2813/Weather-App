40e8820fed6690d3711e684249365969607aabc1
SandraLima2813 <sandra.c.nicolau@gmail.com>
2021-03-07 13:02:17 +0100

    Added forecast

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 let now = new Date();
 
+
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
 let currentDate = document.querySelector("#currentDate");
 let date = now.getDate();
 let hours = now.getHours();
@@ -63,12 +78,42 @@ function displayWeatherCondition(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);   
 }
 
+function displayForecast (response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+   <div class="col-2">
+      <h3>
+        ${formatHours(forecast.dt * 1000)}
+      </h3>
+      <img
+        src="http://openweathermap.org/img/wn/${
+          forecast.weather[0].icon
+        }@2x.png"
+      />
+      <div class="weather-forecast-temperature">
+        <strong>
+          ${Math.round(forecast.main.temp_max)}°
+        </strong>
+        ${Math.round(forecast.main.temp_min)}°
+      </div>
+    </div>  `
+  }
+}
+
 function search(event) {
   event.preventDefault();
   let apiKey = "566f369c90a6b3714753dfafe16abfce";
   let city = document.querySelector("#city-input").value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`; 
+  axios.get(apiUrl). then(displayForecast);
 }
 
 let searchForm = document.querySelector("#search-form");
@@ -114,6 +159,4 @@ let celsiusTemperature = null;
 let currentLocationButton = document.querySelector("#current-location");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
-
-
-
+search ("Porto"); 
